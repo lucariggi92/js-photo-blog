f142e5ecdcf6a3893d1b81d20e7abd1ce068673b
lucariggi92 <riggiluca92@gmail.com>
2025-11-19 16:55:31 +0100

    feat: milestone 1 completed

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 
 const cardsGrid = document.getElementById("cardsGrid");
 
+
+//richiamo un array con oggetti. ogni oggetto è una card con info all'interno
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then(function (resp) {
-        const cardArray = resp.data;
+        
+    //sto assegnando alla variabile cardArray un valore (una serie di oggetti)
+    const cardArray = resp.data;
+
         let cardStr = "";
+
+       // voglio estrarre dall'array dei valori uso il foreach
         cardArray.forEach((curCard) => {
+        //destrutturazione per semplificare l'inserimento delle variabili 
+        // all'interno della stringa
+        
+            const {url, title , date} = curCard;
+         
+           // creo la stringa da inserire nell'HTML
+
             cardStr = cardStr +
                 `<div class="col ">
             <div class="card bg-white">
-          <img src="${curCard.url}" alt="${curCard.title}">
-          <p class="data p-top-15">${curCard.data}</p>
-          <h2 class="titoli">${curCard.title}</h2>
+             <img src="${url}" alt="${title}">
+            <p class="data p-top-15">${date}</p>
+            <h2 class="titoli">${title}</h2>
             </div>
             <img src="./img/pin.svg" alt="" class="pin">
             </div>`
         });
+
+        //print in html le cards
  cardsGrid.innerHTML =cardStr;
 });
